a739ddf8c3f83ec2b583fd3ab522fc89d0d3885f
Kuro <hello@kurojifusky.com>
2025-07-07 00:54:31 +0800

    streamlining ignore list and whitelists

--- scraper/constants.py
+++ scraper/constants.py
@@ -1,12 +1,7 @@
 __all__ = [
     "VILLAINS_BASE_URL",
     "HEROES_BASE_URL",
-    "_WHITELISTED_KEYWORDS"
 ]
 
 VILLAINS_BASE_URL = "villains.fandom.com"
 HEROES_BASE_URL = "hero.fandom.com"
-
-# This is a really scuffed implementation but useful for
-# tracking words from the "Appearance" sections or tracking if no infobox exists
-_WHITELISTED_KEYWORDS = ["fox", "wolf", "werewolf"]
