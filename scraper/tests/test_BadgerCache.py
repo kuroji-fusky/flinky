997cea817f4259c3be6b89dd84a005901278d9ca
Kuro <hello@kurojifusky.com>
2025-07-07 23:53:10 +0800

    add dummy test file

---
+++ scraper/tests/test_BadgerCache.py
@@ -0,0 +1,11 @@
+import unittest
+from ..crawl_cache import BadgerCache  # type: ignore
+
+
+class CacheTest(unittest.TestCase):
+    def stuff(self):
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main()
