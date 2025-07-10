8ef3a4eceb7bd2bba0224d5a09a7980c02d1de3d
Kuro <hello@kurojifusky.com>
2025-07-10 11:41:25 +0800

    add le delay function

--- server/cmd/scraper/scraper.go
+++ server/cmd/scraper/scraper.go
@@ -3,21 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
-func DefineBoolFlags(ptr *bool, flags []string, defaultVal bool, help string) {
+// Adds a N second delay so I don't get my ass rate limited
+func delayRequest(delaySec int) {
+	time.Sleep(time.Duration(delaySec) * time.Second)
+}
+
+func defineBoolFlags(ptr *bool, flags []string, defaultVal bool, help string) {
 	for _, f := range flags {
 		flag.BoolVar(ptr, f, defaultVal, help)
 	}
 }
 
-func DefineIntFlags(ptr *int, flags []string, defaultVal int, help string) {
+func defineIntFlags(ptr *int, flags []string, defaultVal int, help string) {
 	for _, f := range flags {
 		flag.IntVar(ptr, f, defaultVal, help)
 	}
 }
 
-func DefineStringFlags(ptr *string, flags []string, defaultVal string, help string) {
+func defineStringFlags(ptr *string, flags []string, defaultVal string, help string) {
 	for _, f := range flags {
 		flag.StringVar(ptr, f, defaultVal, help)
 	}
@@ -25,18 +31,19 @@ func DefineStringFlags(ptr *string, flags []string, defaultVal string, help stri
 
 func main() {
 	// Flag stuff
+
 	var flagReqDelay int
 	var flagReqTimeout int
 	var flagReqRetries int
 	var flagVerboseMode bool
 	var flagUseDatabase string
 
-	DefineIntFlags(&flagReqDelay, []string{"delay", "d"}, 4, "Delay between requests in seconds, default is 4")
-	DefineIntFlags(&flagReqTimeout, []string{"timeout", "T"}, 15, "Request timeout in seconds, default is 15")
-	DefineIntFlags(&flagReqRetries, []string{"retries", "r"}, 3, "Number of retries for failed requests, default is 3")
+	defineIntFlags(&flagReqDelay, []string{"delay", "d"}, 4, "Delay between requests in seconds, default is 4")
+	defineIntFlags(&flagReqTimeout, []string{"timeout", "T"}, 15, "Request timeout in seconds, default is 15")
+	defineIntFlags(&flagReqRetries, []string{"retries", "r"}, 3, "Number of retries for failed requests, default is 3")
 
-	DefineBoolFlags(&flagVerboseMode, []string{"verbose-mode", "vm"}, false, "Enable verbose mode for debugging")
-	DefineStringFlags(&flagUseDatabase, []string{"use-db", "db"}, "", "Enable database usage")
+	defineBoolFlags(&flagVerboseMode, []string{"verbose", "v"}, false, "Enable verbose mode for debugging")
+	defineStringFlags(&flagUseDatabase, []string{"use-db", "db"}, "", "Enable database usage")
 
 	flag.Parse()
 
@@ -45,4 +52,5 @@ func main() {
 	}
 
 	// WIP rewriting all the scraper logic from Python so there's literally nothing here yet lmao
+	delayRequest(flagReqDelay)
 }
